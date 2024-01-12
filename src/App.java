d897122b2a217cb2decf1a2456c2e43bf3ee1370
DucTran04 <ductran04.hh@gmail.com>
2024-01-12 21:50:46 +0700

    BaiThucHanh

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("bai thuc hanh");
+        System.out.println("bai thuc hanh 1");
     }
 }
 
